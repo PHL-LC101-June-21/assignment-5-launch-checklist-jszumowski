c4a41e4a7c129dc05c8bc0ac2a3992be8ef74b08
Joseph Szumowski <jszumowskijr@gmail.com>
2021-08-26 08:20:30 -0400

    added alerts for fuel level and cargo mass. changed spelling to match the tests

--- scriptHelper.js
+++ scriptHelper.js
@@ -49,16 +49,18 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
     copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     if (fuelLevel < 10000){
-        launchStatus.innerHTML = "Shuttle Not Neady for Naunch";
+        alert('Fuel too low for launch')
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = 'rgb(199, 37, 78)';
         fuelStatus.innerHTML = "Fuel too low for launch";
     }
     if (cargoLevel > 10000){
+        alert('Cargo mass too heavy for launch')
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = 'rgb(199, 37, 78)';
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
     }
-    if (fuelLevel > 9999 && cargoLevel < 10001){
+    if (fuelLevel > 9999 && cargoLevel < 10000){
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
         launchStatus.style.color = 'rgb(65, 159, 106)';
     }
