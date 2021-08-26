3da2e7f8f3ee75681d2b9ff03936b44738f59d70
Joseph Szumowski <jszumowskijr@gmail.com>
2021-08-26 08:24:48 -0400

    removed unnecessary alerts

--- scriptHelper.js
+++ scriptHelper.js
@@ -49,13 +49,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
     copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     if (fuelLevel < 10000){
-        alert('Fuel too low for launch')
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = 'rgb(199, 37, 78)';
         fuelStatus.innerHTML = "Fuel too low for launch";
     }
     if (cargoLevel > 10000){
-        alert('Cargo mass too heavy for launch')
         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
         launchStatus.style.color = 'rgb(199, 37, 78)';
         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
