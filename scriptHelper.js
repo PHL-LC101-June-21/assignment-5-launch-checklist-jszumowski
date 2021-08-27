87207068d6d615eed6a278d25183828821c7e990
Joseph Szumowski <jszumowskijr@gmail.com>
2021-08-27 16:10:20 -0400

    added line 64 and 65 to make sure the code is able to be run multiple times

--- scriptHelper.js
+++ scriptHelper.js
@@ -39,7 +39,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
        alert('All fields required.');
    }
    if (validateInput(fuelLevel) === 'Empty' || validateInput(cargoLevel) === 'Empty'){
-    alert('All fields required.');
+        alert('All fields required.');
     }
    if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number'){
        alert('Fuel and Cargo Level must be numbers');
@@ -61,6 +61,8 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     if (fuelLevel > 9999 && cargoLevel < 10000){
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
         launchStatus.style.color = 'rgb(65, 159, 106)';
+        fuelStatus.innerHTML = "Fuel level high enough for launch";
+        cargoStatus.innerHTML = "Cargo mass low enough for launch";
     }
 }
 
