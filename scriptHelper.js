07043f6a199750b352cd0f043f917fa1d40230ac
Joseph Szumowski <jszumowskijr@gmail.com>
2021-08-28 08:36:27 -0400

    added an additional equal sign

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,7 +17,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    if (testInput == ''){
+    if (testInput === ''){
         return 'Empty';
     }
     if (isNaN(testInput)){
